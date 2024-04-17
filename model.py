a360ca2b908c117b28623f52b7e2be647214bccc
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-17 11:37:14 +0800

    parent generation working

--- model.py
+++ model.py
@@ -7,12 +7,12 @@ class RationaLLM():
         Model
     """
     def __init__(self):
-        self.hi = 'hi'
+        pass
 
     def get_nodes(text):
         # instruction modified from https://arxiv.org/pdf/2309.11392.pdf
-        instruction = 'I want you to act as a language expert. Your task is to extract concise and relevant factual statements from the text. Include only statements that have a truth value and are worth validating, and ignore subjective claims. You should generate a bullet list of statements that are potentially true or false. Please only reply with the bullet list and nothing else.'
-        response = get_response(text, instruction)
+        instruction = 'I want you to act as a language expert. Your task is to extract concise and relevant statements from the text. The truthfulness of the statement is irrelevant. Please only reply with the bullet list and nothing else.'
+        response = get_response(text, instruction, 0)
         unformatted_nodes = response.split('\n')
         nodes = []
         punc = ".,!?"
@@ -24,17 +24,28 @@ class RationaLLM():
             
         return nodes
     
-    def edge_finder(text, node1, node2):
-        statement1 = f'The fact that {node1} implies that {node2}'
-        statement2 = f'The fact that {node2} implies that {node1}'
-        response = get_boolean_completion(text, statement1)
-        if response[0] == '1': return 1
-        response = get_boolean_completion(text, statement2)
-        if response[0] == '1': return 2
-        return 0
+    def gen_parent(text, child, supporting=True):
+        if supporting:
+            instruction = f'You believe everything in the following text to be true. {text}'
+            message = f"{child}. Explain the last statement with a 2-step reasoning chain. Each step must be a short statement."
+        else:
+            instruction = f'You believe everything in the following text to be false. {text}'
+            message = f"{child}. Explain why the the last statement is false with a 2-step reasoning chain. Each step must be a short factual statement."
+        
+        parent = get_response(message, instruction, 0.6)
+
+        # formatting
+        parent = parent.split('\n')[0].split(':')[-1].strip()
+        punc = ".,!?"
+        if parent[-1] in punc: parent = parent[:-1]
+
+        # check if model believes parent implies child
+        response = get_boolean_completion(f'{parent[:-1]}. This implies that {child}.')
+        if response == None: return None
+
+        truth_val, probs = response
+        return parent, probs
     
-    def gen_parent(text, child):
-        pass
 
         
     
--- utils.py
+++ utils.py
@@ -1,42 +1,47 @@
 from openai import AzureOpenAI
 from client import client
+from math import exp
 
-def get_response(message, instruction):
+def get_response(message, instruction, temp):
     response = client.chat.completions.create(
 		model = 'gpt-35-turbo',
-        temperature = 0,
+        temperature = temp,
         messages = [
             {"role": "system", "content": instruction},
             {"role": "user", "content": message}
         ]
-    )
-    
+    ) 
     # print token usage
     # print(response.usage)
 
     return response.choices[0].message.content
 
-def get_boolean_completion(text, statement):
+def get_boolean_completion(statement, text=None):
+    if text: 
+        prompt = f'Text: {text}\nYou are an expert labelling bot. Given the previous text, label the following statement with boolean value 0 or 1.\nStatement: {statement}\nLabel: '
+    else:
+        prompt = f'You are an expert labelling bot. Label the following statement with boolean value 0 or 1 indicating its truthfulnes.\nStatement: {statement}\nLabel: ', 
     response = client.completions.create(
 		model = 'gpt-35-turbo',
-        prompt = f'Text: {text}\nYou are an expert labelling bot. Given the previous text, label the following statement with boolean value 0 or 1.\nStatement: {statement}\nLabel: ',
+        prompt = prompt,
         temperature = 0,
         max_tokens = 1,
         seed = 8,
         logprobs = 2,
     )
-    
-    return response.choices[0].text, response.choices[0].logprobs.top_logprobs
 
-def get_reasoning(text, statement):
-    response = client.chat.completions.create(
-        model = 'gpt-35-turbo',
-        temperature = 0,
-        messages = [
-            {"role": "system", "content": text},
-            {"role": "user", "content": f"{statement}. Explain the last statement with one fact."}
-        ]
-    )
+    # assert answer is 0 or 1
+    answer = response.choices[0].text
+    if answer != '0' and answer != '1': return None
 
-    return response.choices[0].message.content
+    # format logprobs and calculate probabilities
+    logprobs = response.choices[0].logprobs.top_logprobs[0]
+    if '1' in logprobs and '0' in logprobs:
+        log0 = exp(logprobs['0'])
+        log1 = exp(logprobs['1'])
+        sum = log0 + log1
+        return int(answer), [log0/sum, log1/sum]
+    return None
+
+
 
