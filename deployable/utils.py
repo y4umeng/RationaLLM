d24924246b3354ddcffdfa189d0f15cce6157b56
Jacob Davis <jacobdavis799@gmail.com>
2024-05-23 16:19:59 +0100

    Boolean fix

--- deployable/utils.py
+++ deployable/utils.py
@@ -23,32 +23,22 @@ def get_response(message, instruction, temp = 0.6):
     return response.choices[0].message.content
 
 def get_boolean_completion(statement, text=None):
-    if text: 
-        prompt = f'Text: {text}\nYou are an expert labelling bot. Given the previous text, label the following statement with boolean value 0 or 1.\nStatement: {statement}\nLabel: '
-    else:
-        prompt = f'You are an expert labelling bot. Label the following statement with boolean value 0 or 1 indicating its truthfulnes.\nStatement: {statement}\nLabel: '
-    response = client.chat.completions.create(
-        model = 'gpt-35-turbo',
-        messages = [{"role": "user", "content": prompt}],
-        temperature = 0,
-        max_tokens = 1,
-        seed = 8,
-        logprobs = True,
-        top_logprobs = 2,
-    )
+    if not text:
+        raise 'must supply text'
 
-    # assert answer is 0 or 1
-    answer = response.choices[0].text
-    if answer != '0' and answer != '1': return None
+    inference = classifier('[CLS]' + text + '[SEP]' + statement + '[SEP]')[0]
 
-    # format logprobs and calculate probabilities
-    logprobs = response.choices[0].logprobs.top_logprobs[0]
-    if '1' in logprobs and '0' in logprobs:
-        log0 = exp(logprobs['0'])
-        log1 = exp(logprobs['1'])
-        sum = log0 + log1
-        return int(answer), [log0/sum, log1/sum]
-    return None
+    #Set probabilities
+    log0 = inference['score']
+    log1 = (1-inference['score'])
+
+    #Set the chosen class
+    if inference['label'] == 'CONTRADICTION' or inference['label'] == 'NEUTRAL':
+        answer = 0
+    elif inference['label'] == 'ENTAILMENT':
+        answer = 1
+
+    return int(answer), [log0, log1]
 
 #Say whether the statement is inferencable by the text
 def get_opinion(statement, text):
